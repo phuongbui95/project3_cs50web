b1a03b287efa75b33dc478a18a7f1bb19a2f5059
PB Github <phuong.buitan95@gmail.com>
2023-04-26 11:53:01 +0700

    login: username=email

--- mail/mail/admin.py
+++ mail/mail/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import User, Email
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id","email","username")
+
+class EmailAdmin(admin.ModelAdmin):
+    list_display = ("id","sender","subject","body","timestamp","read","archived")
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Email, EmailAdmin)
