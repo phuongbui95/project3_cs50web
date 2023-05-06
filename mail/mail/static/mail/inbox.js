a2788edf0628587fcd1c6decc774e355c5374962
PB Github <phuong.buitan95@gmail.com>
2023-05-06 14:24:01 +0700

    add buttons

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -95,14 +95,14 @@ function get_mailbox(mailbox) {
     let latest10SortedEmails = sortedEmails.slice(0,10);
     latest10SortedEmails.forEach(email => {
       //-- Display overview of email's content in emails-view of mailbox
-      list_emails(email.sender, email.subject, email.timestamp, email.read, email.id);
+      list_emails(email.sender, email.subject, email.timestamp, email.read, email.id, mailbox);
     });
     return;
   });
 }
 
 //
-function list_emails(senderVar, subjectVar, timestampVar, readVar, idVar) {
+function list_emails(senderVar, subjectVar, timestampVar, readVar, idVar, mailbox) {
   // Create div tag to contain email's content
   const email_div = document.createElement('div');
   email_div.className= 'overview';
@@ -122,12 +122,12 @@ function list_emails(senderVar, subjectVar, timestampVar, readVar, idVar) {
     // Show in-email-view and hide other views
     document.querySelector('#emails-view').style.display = 'none';
     document.querySelector('#compose-view').style.display = 'none';
-    view_email(idVar);
+    view_email(idVar, mailbox);
   });
 }
 
 // GET particular email from email_id
-function view_email(email_id) {
+function view_email(email_id, mailbox) {
   // get emails by email_id
   
   fetch(`/emails/${email_id}`)
@@ -140,18 +140,102 @@ function view_email(email_id) {
      * Subject: subject
      * --> add a horizontal break line <hr> here!
      * Show the 'body' of email here
+     * 
+     * // Function
+     * Update email.read by PUT method to 'true'.
+     * Present email'content as above.
+     * Add 'archive' button for 'inbox' email, 'unarchive' one for 'archive' email.
+     * Add 'reply' button for 'inbox' email. 
      */
-    
-    // Show in-email-view
+
+    // Update this email is read
+    fetch(`/emails/${email_id}`,{
+      method: 'PUT',
+      body: JSON.stringify({
+        read: true
+      })
+    })
+
+    //--- Show in-email-view ---//
     let emailView = document.querySelector('#in-email-view');
     emailView.style.display = 'block';
-    emailView.innerHTML = `From: ${email.sender}<br>
+    // Archive button for emails in 'inbox' mailbox
+    /**
+     * Create a 'Archive' button
+     * console.log('Click on Archive button');
+     * If button is pressed, append this email to 'archive' mailbox
+     */
+    let archive_button = document.createElement('BUTTON');
+    archive_button.setAttribute('id','archive');
+    let archive_textnode = document.createTextNode('Archive');
+    archive_button.appendChild(archive_textnode);
+    
+    archive_button.addEventListener('click', () => {
+      console.log('Click on archive_button');
+    });
+
+    // Unarchive button for emails in 'archive' mailbox
+    /**
+     * Create a 'Unarchive' button
+     * console.log('Click on Unarchive button');
+     * If button is pressed, append this email to 'inbox' mailbox
+     */
+    let unarchive_button = document.createElement('BUTTON');
+    unarchive_button.setAttribute('id','unarchive');
+    let unarchive_textnode = document.createTextNode('Unarchive');
+    unarchive_button.appendChild(unarchive_textnode);
+    
+    unarchive_button.addEventListener('click', () => {
+      console.log('Click on unarchive_button');
+    });
+
+    // Reply button
+    /**
+     * Create a 'Reply' button
+     * console.log('click on Reply');
+     */
+    let reply_button = document.createElement('BUTTON');
+    reply_button.setAttribute('id','reply');
+    let reply_textnode = document.createTextNode('Reply');
+    reply_button.appendChild(reply_textnode);
+    reply_button.addEventListener('click', () => {
+      console.log('Click on reply_button');
+    });
+
+    // class = intro-view
+    let intro_div = document.createElement('div')
+    intro_div.className = "intro-view";
+    intro_div.innerHTML = `From: ${email.sender}<br>
                           To: ${email.recipients}<br>
                           Subject: ${email.subject}<br>
                           Timestamp${email.timestamp}<br>
-                          <hr>
-                          ${email.body}
                           `;
+    intro_div.addEventListener('click', () => {
+      console.log('Click on intro_div');
+    });
+    // class = body-view
+    let body_div = document.createElement('div')
+    body_div.className = "body-view";
+    body_div.innerHTML = `<hr>${email.body}`;
+    body_div.addEventListener('click', () => {
+      console.log('Click on body_div');
+    });
+
+    // add elements to emailView
+    let emailView_array = [intro_div,reply_button
+                          ,archive_button,unarchive_button
+                          ,body_div];
+    emailView_array.forEach(view => emailView.appendChild(view));
+    
+    // button customization
+    // if(mailbox == 'sent'){
+    //   document.querySelector('#archive').style.display='none';
+    //   document.querySelector('#unarchive').style.display='none';
+    // } else if(mailbox == 'inbox'){
+    //   document.querySelector('#unarchive').style.display='none';
+    // } else {
+    //   document.querySelector('#archive').style.display='none';
+    // }
     return;
   });
 }
