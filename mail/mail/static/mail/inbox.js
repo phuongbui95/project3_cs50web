f92997010a53e58041e6ab74353c623c5a6a151a
PB Github <phuong.buitan95@gmail.com>
2023-05-12 12:50:34 +0700

    Adjust the div tags

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -22,7 +22,7 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
-
+  
   //////------ Redirect to 'sent' mailbox when submit is clicked
   document.querySelector('form').onsubmit = (event) => {
     // send value from form's fields and trigger POST api
@@ -47,7 +47,7 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `
-                                                      <div id="${mailbox}_div">
+                                                      <div id="${mailbox}_listing">
                                                         <h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>
                                                       </div>
                                                       `;
@@ -90,55 +90,76 @@ function get_mailbox(mailbox) {
     let sortedEmails = emails.sort((a,b) => b.timestamp - a.timestamp);
     
     ///// Filter emails for particular mailbox
-    let filteredEmails = [];
-    if(mailbox == 'inbox') {
-      filteredEmails = sortedEmails.filter(email => email.archived === false);
+    // let filteredEmails = [];
+    // if(mailbox == 'inbox') {
+    //   filteredEmails = sortedEmails.filter(email => email.archived === false);
       
-    } else if(mailbox == 'archive') {
-      filteredEmails = sortedEmails.filter(email => email.archived === true);
+    // } else if(mailbox == 'archive') {
+    //   filteredEmails = sortedEmails.filter(email => email.archived === true);
       
-    } else {
-      filteredEmails = sortedEmails;
+    // } else {
+    //   filteredEmails = sortedEmails;
       
-    }
-        
+    // }
+    const filteredEmails = sortedEmails.filter(email => {
+      switch (mailbox) {
+        case 'inbox':
+          return email.archived === false;
+        case 'archive':
+          return email.archived === true;
+        default:
+          return true;
+      }
+    });
+
     // Show emails in #emails-view
-    let latestfilteredEmails = filteredEmails.slice(0,10);
+    let latestfilteredEmails = filteredEmails.slice(0,20);
     // console.log(latestfilteredEmails);
     latestfilteredEmails.forEach(email => {
       //-- Display overview of email's content in emails-view of mailbox
-      list_emails(email.sender, email.subject, email.timestamp, email.read, email.id, mailbox);
+      list_email(email.sender, email.subject, email.timestamp, email.read, email.id, mailbox);
     });
     return;
   });
 }
 
 //
-function list_emails(senderVar, subjectVar, timestampVar, readVar, idVar, mailbox) {
+function list_email(senderVar, subjectVar, timestampVar, readVar, idVar, mailbox) {
   // parent dive to store listed emails
-  const target_mailbox_div = document.querySelector(`#emails-view #${mailbox}_div`);
+  const target_mailbox_div = document.querySelector(`#emails-view #${mailbox}_listing`);
   // Create div tag to contain email's content
   const email_div = document.createElement('div');
   const h3_emailsView = document.querySelector('#emails-view h3'); // Identify the h3 tag in #emails-view
-  email_div.className = h3_emailsView.innerHTML; //Assign h3 tag's text as email_div's className
-  email_div.setAttribute('id',`email_${idVar}`);
+  email_div.className = h3_emailsView.innerHTML + ` email_${idVar}`;
   email_div.innerHTML = `${senderVar}, ${subjectVar}, ${timestampVar}<br>`; //add <br> to see line breaks
   // Append email_div to parent div in mailbox
   target_mailbox_div.appendChild(email_div);
   // Add border to target mailbox
   email_div.style.border = "1px solid black";
   // Change background's color of read emails
-  if(readVar) {
-  email_div.style.backgroundColor = "gray";
-  } else {
-  email_div.style.backgroundColor = "white";
+  email_div.style.backgroundColor = readVar ? "gray": "white";
+  // Check whether to create a new div in #in-email-view?
+  if(!document.querySelector(`#email_${idVar}`)) {
+    let createDiv = document.createElement('div');
+    createDiv.setAttribute('id', `email_${idVar}`);
+    document.querySelector(`#in-email-view #${mailbox}-email`).appendChild(createDiv);
   }
+  
+  document.querySelector(`#email_${idVar}`).style.display = 'none';
 
   // Event when a div element containing email is clicked
   email_div.addEventListener('click', () => {
     // Show in-email-view and hide other views
     document.querySelector('#emails-view').style.display = 'none';
     document.querySelector('#compose-view').style.display = 'none';
+    document.querySelector('#in-email-view').style.display = 'block'; //parent node level 1
+    for (const box of ['inbox', 'sent', 'archive']) { //parent node level 2
+      document.querySelector(`#${box}-email`).style.display = (box === mailbox) ? 'block' : 'none';
+    }
+
+    document.querySelector(`#email_${idVar}`).style.display = 'block'; // child node
+
+    // view particular email based on email's id
     view_email(idVar, mailbox);
   });
 }
@@ -172,41 +193,45 @@ function view_email(email_id, mailbox) {
         read: true
       })
     })
+    
+    //--- Show in-email-view of particular email_id---//
+    const emailDiv = document.querySelector(`#email_${email_id}`);
 
-    //--- Show in-email-view ---//
-    let emailView = document.querySelector('#in-email-view');
-    emailView.style.display = 'block';
-    let email_intro_view = emailView.querySelector('.intro-view');
-    email_intro_view.innerHTML =
-    `
-      From: ${email.sender}<br>
-      To: ${email.recipients}<br>
-      Subject: ${email.subject}<br>
-      Timestamp${email.timestamp}<br>   
+    emailDiv.innerHTML = `
+      <div class="intro-view">
+        From: ${email.sender}<br>
+        To: ${email.recipients}<br>
+        Subject: ${email.subject}<br>
+        Timestamp: ${email.timestamp}<br>
+      </div>
+      <div class="buttons-view">
+        <button class="replyButton">Reply</button>
+        <button class="archiveButton">Archive</button>
+        <button class="unarchiveButton">Unarchive</button>
+        <hr>
+      </div>
+      <div class="body-view">
+        <p>
+          ${email.body.replace(/\[Input reply below\]/g, '<hr>')}
+        </p>
+      </div>
     `;
-    let email_buttons_view = emailView.querySelector('.buttons');
-    email_buttons_view.innerHTML =
-    `
-    <button id="replyButton">Reply</button>
-    <button id="archiveButton">Archive</button>
-    <button id="unarchiveButton">Unarchive</button>
-    `;
-    let email_firstBody_view = emailView.querySelector('.body-view .first-body');
-    email_firstBody_view.innerHTML =`<hr>${email.body}`;
                 
     // Hide the expected buttons
-    if(mailbox == 'sent'){
-      document.querySelector('#archiveButton').style.display='none';
-      document.querySelector('#unarchiveButton').style.display='none';
-    } else if(mailbox == 'inbox'){
-      document.querySelector('#unarchiveButton').style.display='none';
-    } else {
-      document.querySelector('#archiveButton').style.display='none';
+    let buttons = {
+      'sent': ['archiveButton', 'unarchiveButton'],
+      'inbox': ['unarchiveButton'],
+      'archive': ['archiveButton', 'replyButton'],
+    };
+    
+    for(let value of buttons[mailbox] ) {
+      document.querySelector(`#email_${email_id} .${value}`).style.display = 'none';
     }
+    
 
     // Trigger the buttons
     // Click on archive
-    document.querySelector('#archiveButton').onclick = () => {
+    document.querySelector('.archiveButton').onclick = () => {
       console.log('Clicked on Archive');
       // Update archive = true
       fetch(`/emails/${email_id}`, {
@@ -224,7 +249,7 @@ function view_email(email_id, mailbox) {
     }
 
     // Update archive = false
-    document.querySelector('#unarchiveButton').onclick = () => {
+    document.querySelector('.unarchiveButton').onclick = () => {
       console.log('Clicked on Unarchive');
       // Update archive = false
       fetch(`/emails/${email_id}`, {
@@ -242,42 +267,75 @@ function view_email(email_id, mailbox) {
     }
 
     // Click on Reply
-    document.querySelector('#replyButton').onclick = () => {
+    document.querySelector('.replyButton').onclick = () => {
       console.log('Clicked on Reply');
-      reply_email(email.sender, email.subject, email.timestamp, email.body);
+      reply_email(email.sender
+                  , email.subject
+                  , email.timestamp
+                  , email.body
+                  , email_id
+                  );
     }
     return;
   });
 }
 
-function reply_email(senderVar, subjectVar, timestampVar, bodyVar) {
+function reply_email(senderVar, subjectVar, timestampVar, bodyVar, idVar) {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
   document.querySelector('#in-email-view').style.display = 'none';
 
-  // Pre-fill composition fields to reply the original email
+  // Pre-fill recipients
   document.querySelector('#compose-recipients').value = senderVar;
   const regex = /^Re: /;
+  // Pre-fill subjects
   if(regex.test(subjectVar) == true) {
     document.querySelector('#compose-subject').value = `${subjectVar}`;  
   } else {
     document.querySelector('#compose-subject').value = `Re: ${subjectVar}`;
   } 
-  let reply_body = `\n-----------\n`
-                  +`\nOn ${timestampVar}, ${senderVar} wrote:`
-                  +`\n${bodyVar}\n-----------\n`
-                  + `Reply:\n`
+  // Pre-fill the 1st part of body
+  /**
+   *  Design email_bodyTemplate
+   *  Case 01: Without replies yet
+   *  Case 02: With replies
+   *  
+  */
+  let currentBodyText ='';
+  let firstBodyText = '';
+  let email_bodyTemplate = '';
+  if(document.querySelector(`#in-email-view .body-view .reply-body .email_${idVar}`) === '') {
+    firstBodyText = bodyVar;
+    currentBodyText = firstBodyText;
+  } else {
+    currentBodyText = bodyVar.slice(bodyVar.indexOf('[Input reply below]',bodyVar.indexOf('[Input reply below]')+1)
+                              ,bodyVar.length);
+    firstBodyText = bodyVar.slice(0,bodyVar.indexOf('[Input reply below]'))
+
+  }
+  email_bodyTemplate = firstBodyText
+                  + `\n[Input reply below]\n`
+                  + `On ${timestampVar}, ${senderVar} wrote:`
+                  + `\n${currentBodyText}`
+                  + `\n[Input reply below]\n`
                   ;
-  document.querySelector('#compose-body').value = reply_body;
+  
+
+  //prefill intro to body of reply
+  document.querySelector('#compose-body').value = email_bodyTemplate;
 
   //////------ Submit is clicked
   document.querySelector('form').onsubmit = (event) => {
-    // send value from form's fields and trigger POST api
+    // Send value from form's fields and trigger POST api
     let recipients = document.querySelector('#compose-recipients').value; // use .value to collect data from a Form Submit
     let subject = document.querySelector('#compose-subject').value;
-    let body = document.querySelector('#compose-body').value;
+    let body = document.querySelector('#compose-body').value;//after the reply body is inputted
     post_email(recipients, subject, body);
+    
     
   }
 }
+
+
+
